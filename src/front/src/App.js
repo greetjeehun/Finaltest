62ceaf27789a12fc868da81696c57336d1d3f75d
greetjeehun <greet123@naver.com>
2022-11-01 15:40:43 +0900

    test1

---
+++ src/front/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from "react";
+import axios from "axios";
+
+function App() {
+    const [id,setid]=useState();
+    const selectall=()=>{
+        axios.get("/select")
+            .then((res)=>{
+                console.log(res.data);
+                console.log("000");
+            })
+    }
+  return (
+    <div className="App">
+        <button type={"button"} onClick={selectall}>seaalect</button>
+
+    </div>
+  );
+}
+
+export default App;
